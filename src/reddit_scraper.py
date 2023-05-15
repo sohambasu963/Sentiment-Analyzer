51af0928c38383db293c7690d30ef25479f42ab6
Soham Basu <sohambasu963@gmail.com>
2023-05-15 16:09:01 -0400

    added a reddit scraper module

--- src/reddit_scraper.py
+++ src/reddit_scraper.py
@@ -1,13 +1,21 @@
 import snscrape.modules.reddit as sreddit
 import pandas as pd
 import time
+import praw
+import os
+from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+USER_AGENT = os.getenv("USER_AGENT")
 
 def get_reddit_posts(query, limit):
     posts = []
     num_posts = 0
-    tries = 0
     for post in sreddit.RedditSearchScraper(query).get_items():
-        tries += 1
         try:
             date = post.date
             text = post.body
@@ -17,15 +25,34 @@ def get_reddit_posts(query, limit):
             num_posts += 1
         except AttributeError:
             continue
-        if num_posts >= limit or tries >= limit * 10:
+        if num_posts >= limit:
             break
 
     df = pd.DataFrame(posts, columns=["Date", "Text"])
     print(df)
     return df
 
+def search_reddit(query, limit):
+    reddit = praw.Reddit(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        user_agent=USER_AGENT
+    )
+
+    search_results = reddit.subreddit('all').search(query, limit=limit)
+
+    posts = []
+    for post in search_results:
+        timestamp = datetime.fromtimestamp(post.created_utc).strftime('%Y-%m-%d %H:%M:%S')
+        posts.append({'timestamp': timestamp, 'content': post.selftext})
+
+    df = pd.DataFrame(posts)
+
+    print(df)
+
 if __name__ == "__main__":  
     time_start = time.time()
-    get_reddit_posts("Canadian economy", 10000)
+    search_reddit("Canadian economy", 10)
     time_end = time.time()
     print(f"\nTotal time taken: {time_end - time_start:.2f} seconds")
+
